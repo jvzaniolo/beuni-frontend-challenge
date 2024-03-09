454a3d4bcb590f7e93bda934437ad6fd1b9c40b6
João Vitor <jv.zaniolo@gmail.com>
2024-03-09 11:15:13 -0300

    add inter font family

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from '@remix-run/node'
 import './app.css'
 import {
   Links,
@@ -7,16 +8,35 @@ import {
   ScrollRestoration,
 } from '@remix-run/react'
 
+export const links: LinksFunction = () => [
+  {
+    href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap',
+    rel: 'stylesheet',
+  },
+]
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <Meta />
         <Links />
       </head>
       <body>
+        <header className="container mx-auto px-4 py-6">
+          <div className="text-4xl">
+            <span className="font-semibold">be</span>
+            <span className="font-bold text-orange-500">uni</span>
+          </div>
+        </header>
         {children}
         <ScrollRestoration />
         <Scripts />
